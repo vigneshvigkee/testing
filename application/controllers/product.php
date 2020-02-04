aa85d155bfe1096dbb5c0474a472792a28fb7dc2
vignesh <Vignesh.Jayasankar@sterlingadministration.com>
2020-02-04 11:24:18 +0530

    testing

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -3,6 +3,9 @@ class Product extends CI_Controller{
     function __construct(){
         parent::__construct();
         $this->load->model('product_model');
+
+
+        
     }
      // function image_upload()  
      //  {  
